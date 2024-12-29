eb5cf5192300c940bb9bd643bba4c77a2273cd0f
tkeerthana1996 <tkeerthana1996@gmail.com>
2024-12-29 22:49:41 +0530

    Added methods in class A and created class B

--- src/test/java/com/sample/ClassA.java
+++ src/test/java/com/sample/ClassA.java
@@ -1,5 +1,15 @@
 package com.sample;
 
 public class ClassA {
+  private void method1() {
+	System.out.println("1");
+}
+  
+  private void method2() {
+	  System.out.println("2");
 
+}
+  private void method3() {
+	  System.out.println("3");
+}
 }
